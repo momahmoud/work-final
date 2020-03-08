424742f2ab1a1ea7cbe629d2580eddce8ab75360
unknown <momahmoud1971998@gmail.com>
2020-03-08 21:32:50 +0200

    =work final

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:harvest/Screens/settings_account_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './Screens/splash_screen.dart';
+import './providers/auth.dart';
+import './providers/cart.dart';
+import './providers/orders.dart';
+
+import './Screens/user_products_screen.dart';
+import './Screens/products_overview_screen.dart';
+import './Screens/product_details_screen.dart';
+import './providers/products.dart';
+import './Screens/about_app.dart';
+
+import 'package:provider/provider.dart';
+import './Screens/edit_product_screen.dart';
+import 'Screens/cart_screen.dart';
+import './Screens/order_screen.dart';
+import './Screens/auth_screen.dart';
+import './helpers/custom_route.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  bool seen = preferences.getBool('seen');
+  Widget _screen;
+
+  if(seen == null || seen == false){
+    _screen = SplashScreen();
+  }else{
+    _screen = AuthScreen();
+  }
+  runApp(
+    MyHome(_screen),
+  );
+}
+
+class MyHome extends StatelessWidget {
+  final Widget _screen;
+  MyHome(this._screen);
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(
+            value: Auth(),
+          ),
+          ChangeNotifierProxyProvider<Auth, Products>(
+            builder: (ctx, auth, previousProducts) => Products(
+                auth.token,
+                auth.userId,
+                previousProducts == null ? [] : previousProducts.items),
+          ),
+          ChangeNotifierProvider.value(value: Cart()),
+          ChangeNotifierProxyProvider<Auth, Orders>(
+            builder: (ctx, auth, previousOrders) => Orders(
+                auth.token,
+                auth.userId,
+                previousOrders == null ? [] : previousOrders.orders),
+          ),
+        ],
+        child: Consumer<Auth>(
+          builder: (ctx, auth, _) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'MyShop',
+            theme: ThemeData(
+                primarySwatch: Colors.green,
+                accentColor: Colors.tealAccent,
+                fontFamily: 'lato',
+                pageTransitionsTheme: PageTransitionsTheme(builders: {
+                  TargetPlatform.android: CustomPageTransitionBuilder(),
+                  TargetPlatform.iOS: CustomPageTransitionBuilder(),
+                })),
+            home: auth.isAuth
+                ? ProdactsOverViewScreen()
+                
+                : FutureBuilder(
+                    future: auth.tryAutoLogin(),
+                    builder: (ctx, authResultSnapshot) =>
+                        authResultSnapshot.connectionState ==
+                                ConnectionState.waiting
+                            ? this._screen
+                            : AuthScreen(),
+                  ),
+            routes: {
+              ProductDetailsScreen.routName: (ctx) => ProductDetailsScreen(),
+              OrdersScreen.routeName: (ctx) => OrdersScreen(),
+              CartScreen.routeName: (ctx) => CartScreen(),
+              UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+              EditProductScreen.routeName: (ctx) => EditProductScreen(),
+              AboutApp.routeName: (ctx) => AboutApp(),
+              SettingsAccountScreen.routeName: (ctx) => SettingsAccountScreen(),
+            },
+          ),
+        ));
+  }
+}
---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:harvest/Screens/seting%20screen.dart';
+import 'package:harvest/Screens/settings_account_screen.dart';
+import 'package:harvest/seting_screen_details/conected_with_me.dart';
+
+import '../providers/auth.dart';
+import '../Screens/user_products_screen.dart';
+import 'package:provider/provider.dart';
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(title: Text('مرحبا بالمزارعين '),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          automaticallyImplyLeading: false,),
+          Divider(),
+          ListTile(leading: Icon(Icons.shop),
+          title: Text('Harvest'),
+          onTap: (){
+            Navigator.of(context).pushReplacementNamed("/");
+
+          },),
+//          Divider(),
+//          ListTile(leading: Icon(Icons.payment),
+//            title: Text('المتطلبات'),
+//            onTap: (){
+//              Navigator.of(context).pushNamed(CartScreen.routeName);
+//              //Navigator.of(context).pushReplacement (CustomRoute(builder: (ctx)=>OrderScreen(),));
+//            },),
+          Divider(),
+          ListTile(leading: Icon(Icons.edit),
+            title: Text('ادارة المنتجات'),
+            onTap: (){
+             Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
+
+            },),
+            Divider(),
+          ListTile(leading: Icon(Icons.person),
+            title: Text('ادارة الحساب'),
+            onTap: (){
+             Navigator.of(context).pushReplacementNamed(SettingsAccountScreen.routeName);
+
+            },),
+          Divider(),
+          ListTile(leading: Icon(Icons.phone_forwarded),
+            title: Text('للتواصل معنا'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => new  Conected_me()));
+            },),
+          Divider(),
+          ListTile(leading: Icon(Icons.settings),
+            title: Text('الاعدادات'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => new  seteing_screen()));
+            },),
+
+          Divider(),
+          ListTile(leading: Icon(Icons.exit_to_app),
+            title: Text('تسجيل الخروج'),
+            onTap: (){
+            Navigator.of(context).pop();
+            Navigator.of(context).pushReplacementNamed('/');
+           //   Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
+              Provider.of<Auth>(context,listen: false).logout();
+
+            },)
+        ],
+      ),
+    );
+  }
+}
